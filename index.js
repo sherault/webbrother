f41209b4d3e6737dfcd8d04525c4640a8a5137d6
Stéphane HERAULT <stephane@oneteacher.net>
2019-09-10 23:28:25 +0000

    wip poc

---
+++ index.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const fs = require('fs')
+const uuidv4 = require('uuid/v4')
+const { exec } = require('child_process')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+
+app.post('/', function (req, res) {
+	const imgBase64 = req.body.img;
+	const data = imgBase64.replace(/^data:image\/\w+;base64,/, "");
+	const buf = Buffer.from(data, 'base64');
+	const filename = `/tmp/image-${uuidv4()}.png`
+	console.log(filename)
+	fs.writeFileSync(filename, buf);
+	exec(`${process.env.BROTHER_QL_BIN} print -l 62 ${filename}`, (err) => {
+		if (err) {
+			console.log(err);
+			return;
+		}
+		fs.unlinkSync(filename)
+	})
+	res.send('ok');
+})
+ 
+app.listen(3000)
+
