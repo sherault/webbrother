4ab1c24b9e98e9b4cb97463533b841d96c8e4c9f
Stéphane HERAULT <stephane.herault@aramisauto.com>
2020-03-12 15:40:15 +0100

    fix(index): increase default limit

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const fs = require('fs')
 const uuidv4 = require('uuid/v4')
 const { exec } = require('child_process')
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json(limit: '50mb'))
+app.use(express.urlencoded({ extended: true, limit: '50mb' }))
 app.use(cors())
 
 app.post('/', function (req, res) {
